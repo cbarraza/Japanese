c8e799515f97b397d01cf406dc3b656a54ae395f
CristopherBarraza <cbarraza@alumnos.inf.utfsm.cl>
2020-07-30 16:01:31 -0400

    Add Home message

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+// Material UI
+import Grid from '@material-ui/core/Grid';
+
+const mainGridStyle = {
+  height: '85vh'
+};
+
+const spanStyle = {
+  fontSize: '5em',
+  fontWeight: 'bold'
+};
 
 const Home = props => {
   return (
-    <h1>Home</h1>
+    <Grid container justify="center" alignItems="center" style={mainGridStyle}>
+      <span style={spanStyle}>がんばりましょう！</span>
+    </Grid>
   );
 };
 
