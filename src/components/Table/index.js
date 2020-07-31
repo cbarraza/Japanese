2860ee073d748181e9c31ba09d0bbad3f204c826
CristopherBarraza <cbarraza@alumnos.inf.utfsm.cl>
2020-07-30 23:25:10 -0400

    Add action buttons

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // Material UI
+import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,6 +8,9 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+// Material UI Icon
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 // Components
 import Loading from '../Loading';
 
@@ -34,7 +38,17 @@ const TableComponent = ({ columns, data, loading, onDelete, onEdit, showActions 
                   <TableCell key={`${row.key}-${column.key}`}>{row[column.key]}</TableCell>
                 ))
               }
-              { showActions && <TableCell>Acciones</TableCell> }
+              { 
+                showActions && 
+                <TableCell>
+                  <IconButton onClick={() => { console.log('edit') }}>
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton onClick={() => { console.log('delete') }}>
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell> 
+              }
             </TableRow>
           ))}
         </TableBody>
